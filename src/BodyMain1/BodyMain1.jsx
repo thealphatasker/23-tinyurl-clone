6677ead96d8eb524f654cddeeb289cc14122b522
Alpha Tasker <81807892+thealphatasker@users.noreply.github.com>
2026-02-28 17:43:20 +0500

    u2.1 - main body p1 -half

--- src/BodyMain1/BodyMain1.jsx
+++ src/BodyMain1/BodyMain1.jsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import './BodyMain1.css';
+import React from "react";
+import "./BodyMain1.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const BodyMain1 = () => {
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+import { faHighlighter } from "@fortawesome/free-solid-svg-icons";
+
+function BodyMain1() {
   return (
     <div className="body-part1">
+      {/* Main Body Left Part */}
       <div className="body-p-left">
         <div className="bpl-heading">
-          <h1>URL Shortener, Branded <br /> Short Links & Analytics</h1>
+          <h1>
+            URL Shortener, Branded <br /> Short Links & Analytics
+          </h1>
         </div>
-
 
         <div className="bpl-paragraph">
           <p className="bpl-paragraph1">
-            Welcome to the original link shortener — simplifying the Internet <br />
+            Welcome to the original link shortener — simplifying the Internet{" "}
+            <br />
             through the power of the URL since 2002.
           </p>
 
           <p className="bpl-paragraph2">
             You can use branded domains for fully custom links, track link
-            analytics, <br /> and enjoy other powerful features with our paid plans.
+            analytics, <br /> and enjoy other powerful features with our paid
+            plans.
           </p>
-
         </div>
 
         <div className="bpl-btn">
@@ -32,51 +39,103 @@ const BodyMain1 = () => {
       {/* Right side */}
       <div className="body-p-right">
         <button className="bpr-shorten-btn">
-          <i className="bpr-shorten-btn-icon fa-solid fa-link" style={{ color: '#000000' }}></i>
+          <i
+            className="bpr-shorten-btn-icon fa-solid fa-link"
+            style={{ color: "#000000" }}
+          ></i>
           Shorten a Link
         </button>
         <button className="bpr-qr-btn">
-          <i className="bpr-qr-btn-icon fa-solid fa-qrcode" style={{ color: 'rgb(0, 0, 0)' }}></i>
+          <i
+            className="bpr-qr-btn-icon fa-solid fa-qrcode"
+            style={{ color: "rgb(0, 0, 0)" }}
+          ></i>
           Generate QR Code
         </button>
 
+        {/* Rightside Box */}
         <div className="Rightside-box">
-          <form className="body-p-right-form" onSubmit={(e) => e.preventDefault()}>
-            <label className="bpr-form-label" htmlFor="user-url">
-              <i className="bpr-form-label-icon fa-solid fa-location-arrow" style={{ color: 'rgb(0, 0, 0)' }}></i>
-              Long URL
-            </label>
-            <br />
-            <input className="bpr-form-input" type="text" name="user-rule" id="user-url" placeholder="Paste your long URL here" required />
+          <form
+            className="body-p-right-form"
+            onSubmit={(e) => e.preventDefault()}
+          >
+            {/* Input 1 */}
+            <div className="bpr-form-input1">
+              <label className="bpr-form-label" htmlFor="user-url">
+                <i
+                  className="bpr-form-label-icon fa-solid fa-location-arrow"
+                  style={{ color: "rgb(0, 0, 0)" }}
+                ></i>
+                Long URL
+              </label>
+              <br />
+              <input
+                className="bpr-form-input"
+                type="text"
+                name="user-rule"
+                id="user-url"
+                placeholder="Paste your long URL here"
+                required
+              />
+            </div>
             <br />
             <div className="bottom-input">
               <div className="bpr-form-p2">
                 <label className="bpr-form-label" htmlFor="bpr-form-select1">
-                  <i className="bpr-form-label-icon fa-solid fa-location-arrow" style={{ color: 'rgb(0, 0, 0)' }}></i>
+                  <FontAwesomeIcon
+                    className="bpr-form-label-icon"
+                    icon={faGlobe}
+                    style={{ color: "rgb(0, 0, 0)" }}
+                  />
                   Domain
                 </label>
                 <br />
-                <select className="bpr-form-select" name="bpr-form-select1" id="bpr-form-select1">
+                <select
+                  className="bpr-form-select"
+                  name="bpr-form-select1"
+                  id="bpr-form-select1"
+                >
                   <option value="tinyurl.com">tinyurl.com</option>
                   <option value="theahsan.com">theahsan.com</option>
                 </select>
               </div>
               <div className="bpr-form-p2-input2">
                 <label className="bpr-form-label" htmlFor="alias-url">
-                  <i className="bpr-form-label-icon fa-solid fa-location-arrow" style={{ color: 'rgb(0, 0, 0)' }}></i>
+                  <FontAwesomeIcon
+                    className="bpr-form-label-icon"
+                    icon={faHighlighter}
+                    style={{ color: "rgb(0, 0, 0)" }}
+                  />
                   Alias (optional)
                 </label>
                 <br />
-                <input className="bpr-form-input2" type="text" name="alias-rule" id="alias-url" placeholder="Paste your long URL here" required />
+                <input
+                  className="bpr-form-input2"
+                  type="text"
+                  name="alias-rule"
+                  id="alias-url"
+                  placeholder="Add alias here"
+                  required
+                />
                 <br />
-                <label className="bpr-form-4" htmlFor="alias-url">Must be at least 5 characters</label>
+                <label className="bpr-form-4" htmlFor="alias-url">
+                  Must be at least 5 characters
+                </label>
               </div>
             </div>
           </form>
+          <button type="submit" className="bpr-form-submit-btn">
+            Shorten Link
+          </button>
+          <br />
+          <p className="bpr-form-p-terms">
+            By clicking Shorten Link, you agree with our Terms of Service,
+            Privacy Policy, <br /> and Use of Cookies.
+          </p>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default BodyMain1;
